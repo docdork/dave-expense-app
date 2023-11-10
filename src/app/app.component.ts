5283918b0ad7777bb07737d32311648c1fe9a17c
David_Holliday <dhholliday@icloud.com>
2023-11-10 14:33:03 +0000

    Add floating action button to add an expense.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonIcon],
 })
 export class AppComponent {
   constructor() {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,82 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonButton,
+  IonActionSheet,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonButton,
+    IonActionSheet,
+    NgFor,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+  ],
 })
 export class HomePage {
   constructor() {}
+
+  public expenseContentList = [
+    {
+      description: 'Expense Description',
+      amount: 21.99,
+      picture: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    },
+    {
+      description: 'Another Expense Description',
+      amount: 17.99,
+      picture: 'https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    },
+  ];
+
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 }
